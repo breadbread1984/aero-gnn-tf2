4d155f59882fa0aa76ee6d86c94e25f28e62ab98
breadbread1984 <breadbread1984@163.com>
2024-02-20 16:17:01 +0800

    update

--- models.py
+++ models.py
@@ -27,7 +27,7 @@ class UpdateZ(tf.keras.layers.Layer):
     if self.k != 0:
       # NOTE: get previous calculated hidden
       z = tfgnn.keras.layers.Readout(from_context = True, feature_name = tfgnn.HIDDEN_STATE)(graph) # z.shape = (node_num, head, channels // head)
-      z_scale = z * tf.log((self.lambd / self.k) + (1 + 1e-6))
+      z_scale = z * tf.math.log((self.lambd / self.k) + (1 + 1e-6))
       h = tf.concat([h, z_scale], axis = -1) # h.shape = (node_num, head, channels // head * 2)
     h = tf.reshape(h, (-1, self.head, (self.channels // self.head) if self.k == 0 else (self.channels // self.head * 2))) # hidden.shape = (node_num, head, channels // head)
     hop_attention = tf.nn.elu(h) # hop_attention.shape = (node_num, head, channels // head)
@@ -50,14 +50,24 @@ class UpdateZ(tf.keras.layers.Layer):
     return cls(**config)
 
 class Propagate(tf.keras.layers.Layer):
-  def __init__(self, **kwargs):
+  def __init__(self, k, **kwargs):
     super(Propagate, self).__init__()
     self.channels = kwargs.get('channels', 64)
     self.head = kwargs.get('head', 1)
+    self.lambd = kwargs.get('lambd', 1.)
+    self.k = k
   def build(self, input_shape):
-    pass
+    self.att = self.add_weight(name = 'att', shape = (1, self.head, self.channels // self.head), trainable = True)
   def call(self, graph, edge_set_name):
-    
+    z = tfgnn.keras.layers.Readout(from_context = True, feature_name = tfgnn.HIDDEN_STATE)(graph)
+    z_scale = z * tf.math.log((self.lambd / self.k) + (1 + 1e-6)) # z_scale.shape = (node_num, head, channel // head)
+    z_scale_i = tfgnn.broadcast_node_to_edges(graph, edge_set_name, tfgnn.SOURCE, feature_value = z_scale) # z_scale_i.shape = (edge_num, head, channel // head)
+    z_scale_j = tfgnn.broadcast_node_to_edges(graph, edge_set_name, tfgnn.TARGET, feature_value = z_scale) # z_scale_j.shape = (edge_num, head, channel // head)
+    a_ij = z_scale_i + z_scale_j
+    a_ij = tf.nn.elu(a_ij) # a_ij.shape = (edge_num, head, channel // head)
+    a_ij = tf.math.reduce_sum(self.att * a_ij, axis = -1) # a_ij.shape = (edge_num, head)
+    a_ij = tf.math.softplus(a_ij) + 1e-6
+    # TODO
 
 def AEROGNN(channels = 64, head = 1, lambd = 1., layer_num = 10, drop_rate = 0.6):
   inputs = tf.keras.Input(type_spec = graph_tensor_spec())
